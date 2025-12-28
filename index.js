637378ee9e0d4ae6ad508163a1e8f69398cdd3f5
Muhammad Furqan Abbas <mfurqanabbas20@gmail.com>
2025-12-28 16:02:01 +0500

    fix: add js from ts and add package file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export const capitalizeWord = (word: string): string => {
+export const capitalizeWord = (word) => {
     const firstLetter = word.charAt(0).toUpperCase();
     const remainingWords = word.slice(1);
     return firstLetter + remainingWords;
