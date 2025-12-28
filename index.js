3c40f09e76e423ef085b75ff72878ddb3381b723
Muhammad Furqan Abbas <mfurqanabbas20@gmail.com>
2025-12-28 16:40:22 +0500

    fix: not picking function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-export default capitalizeWord = (word) => {
+const capitalizeWord = (word) => {
     const firstLetter = word.charAt(0).toUpperCase();
     const remainingWords = word.slice(1);
     return firstLetter + remainingWords;
 }
+
+export default capitalizeWord;
